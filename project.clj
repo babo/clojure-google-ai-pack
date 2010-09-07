92de409329fa6f531cc16e8b53162fd1b1e53002
Attila Babo <ab@issuu.com>
2010-09-08 00:12:45 +0200

    Generate MyBot.jar when use lein uberjar. Add license information.

--- project.clj
+++ project.clj
@@ -1,8 +1,12 @@
 (defproject MyBot "1.0"
-  :description "FIXME: write"
+  :description "Clojure language pack"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :uberjar-name "MyBot.jar"
   :aot [bot planetwars]
   :main bot
+  :disable-implicit-clean true
 )
